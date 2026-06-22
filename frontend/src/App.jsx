29c841bd1770547c4e38d646c26a833a5a092b71
Pranshu Samadhiya <samadhiya.pran@gmail.com>
2026-06-23 01:42:14 +0530

    wallet connection update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { ethers } from "ethers";
 
+import MyNFTArtifact from "./contract/MyNFT.json"
+import MarketplaceArtifact from "./contract/NFTMarketplace.json"
 
 
 const App = () => {
 
   const [wallet, setWallet] = useState("");
   const [nft, setNft] = useState(null);
+  const [marketplace, setMarketplace] = useState(null);
 
+  const NFT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const MARKETPLACE_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
   async function connectWallet() {
     const accounts = await window.ethereum.request({
       method: "eth_requestAccounts",
@@ -17,11 +22,18 @@ const App = () => {
     const provider = new ethers.BrowserProvider(window.ethereum);
     const signer = await provider.getSigner();
     const nftContract = new ethers.Contract(
-      address,
-      abi,
+      NFT_ADDRESS,
+      MyNFTArtifact.abi,
       signer
     );
     setNft(nftContract);
+    
+    const marketplaceContract = new ethers.Contract(
+      MARKETPLACE_ADDRESS,
+      MarketplaceArtifact.abi,
+      signer
+    );
+    setMarketplace(marketplaceContract);
   }
   return (
     <div>
