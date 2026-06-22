43a844d31044bbc8c0a9d0c6e92d4f52e4a3dec3
Pranshu Samadhiya <samadhiya.pran@gmail.com>
2026-06-22 23:56:20 +0530

    contract completed

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -4,82 +4,111 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract NFTMarketplace {
 
-    IERC721 public nft;
+    // IERC721 public nft;
 
     struct Listing{
         address seller;
         uint256 price;
     }
 
-    mapping(uint256 => Listing) public listings;
+    // mapping(uint256 => Listing) public listings;
+    mapping(
+        address => 
+           mapping(
+              uint256 => Listing
+           )
+    ) public listings;
+    // now we'll use like listing[nftAddress][tokenId]
 
-    constructor(address nftAddress){
-        nft = IERC721(nftAddress);
-    }
+    // constructor(address nftAddress){
+    //     nft = IERC721(nftAddress);
+    // }
 
     event NFTListed(
+        address nftContract,
         uint256 tokenId,
         address seller, 
         uint256 price
     );
     event NFTSold(
+        address nftContract,
         uint256 tokenId,
         address buyer
     );
     event NFTCancelled(
+        address nftContract,
         uint256 tokenId
     );
 
-    function listNFT(uint256 tokenId, uint256 price) public {
-        require(price>0, "Prize must be greater than 0" );
-        require(nft.ownerOf(tokenId) == msg.sender, "You are not nft owner" );
-        require(nft.getApproved(tokenId) == address(this), "Marketplace not approved" );
+    function listNFT(
+        address nftContract,
+        uint256 tokenId, 
+        uint256 price
+        ) public {
+            IERC721 nft = IERC721(nftContract);
 
-        listings[tokenId] = Listing(msg.sender, price);
+            require(price>0, "Prize must be greater than 0" );
+            require(nft.ownerOf(tokenId) == msg.sender, "You are not nft owner" );
+            require(nft.getApproved(tokenId) == address(this), "Marketplace not approved" );
 
-        emit NFTListed(
-            tokenId,
-            msg.sender,
-            price
-        );
-    }
+            listings[nftContract][tokenId] = Listing(msg.sender, price);
 
-    function buyNFT(uint256 tokenId) public payable {
-        Listing memory listing = listings[tokenId];
-        require(listing.seller != address(0),"NFT not listed" );
-        // kyuki listed nahi hai to default address 0 ka hi hota hai 
+            emit NFTListed(
+                nftContract,
+                tokenId,
+                msg.sender,
+                price
+            );
+        }
 
-        require(msg.value == listing.price, "Incorrect prize");
+    function buyNFT(
+        address nftContract,
+        uint256 tokenId
+        ) public 
+          payable {
+            IERC721 nft = IERC721(nftContract);
 
-        // before sending eth : verify seller still owns eth ?
-        require(nft.ownerOf(tokenId) == listing.seller, "Seller no longer owns NFT");
-        
-        (bool success, )=
-          payable(listing.seller).call{
-            value: listing.price
-          }("");
-        require(success, "ETH transfer failed");
-
-        nft.safeTransferFrom(
-            listing.seller, 
-            msg.sender, 
-            tokenId
-        );
-        delete listings[tokenId];
-
-        emit NFTSold(
-            tokenId,
-            msg.sender
-        );
+            Listing memory listing = listings[nftContract][tokenId];
+            require(listing.seller != address(0),"NFT not listed" );
+            // kyuki listed nahi hai to default address 0 ka hi hota hai 
+  
+            require(msg.value == listing.price, "Incorrect prize");
+  
+            // before sending eth : verify seller still owns eth ?
+            require(nft.ownerOf(tokenId) == listing.seller, "Seller no longer owns NFT");
+       
+            (bool success, )=
+              payable(listing.seller).call{
+                 value: listing.price
+             }("");
+            require(success, "ETH transfer failed");
+  
+            nft.safeTransferFrom(
+                   listing.seller, 
+                msg.sender, 
+              tokenId
+                );
+            delete listings[nftContract][tokenId];  
+            emit NFTSold(
+                nftContract,
+                tokenId,
+                msg.sender
+             );
     }
     
-    function cancelListing(uint256 tokenId) public {
-        Listing memory listing = listings[tokenId];
+    function cancelListing(
+        address nftContract,
+        uint256 tokenId
+        ) public {
+            // IERC721 nft = IERC721(nftContract);
+
+        Listing memory listing = listings[nftContract][tokenId];
         require(listing.seller != address(0), "not listed");
         require(listing.seller == msg.sender, "not owner");
-        delete listings[tokenId];
+        delete listings[nftContract][tokenId];
 
         emit NFTCancelled(
+            nftContract,
             tokenId
         );
     }
